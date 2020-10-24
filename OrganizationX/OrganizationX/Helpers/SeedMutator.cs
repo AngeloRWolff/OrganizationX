81dafc8d431277a473579079047199a4e32747d3
AngeloRWolff <45687159+AngeloRWolff@users.noreply.github.com>
2020-10-24 09:59:45 +0200

    Updated SeedMutator
    +Added Method 'GetEmployees'
    +Added Method 'Bind'

--- OrganizationX/OrganizationX/Helpers/SeedMutator.cs
+++ OrganizationX/OrganizationX/Helpers/SeedMutator.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace OrganizationX.Helpers
@@ -11,11 +12,42 @@ namespace OrganizationX.Helpers
         public string SeedData { get; set; }
         public SeedOptions Options { get; set; }
 
+        // Mutates Employee Seed Data Into Queue Type Of Employee Model
         public Queue<Employee> GetEmployees()
         {
             Queue<Employee> newEmployees = new Queue<Employee>();
+            CharMapper charMap = new CharMapper();
+
+            foreach(char character in SeedData)
+            {
+                if (character == '\n')
+                {
+                    newEmployees.Enqueue(Bind(charMap.Row));
+                    charMap.ClearRow();
+                }
+                charMap.Append(character);
+            }
+            newEmployees.Enqueue(Bind(charMap.Row)); //EOF
 
             return newEmployees;
+        }
+
+        // Binds Row Of Data To Employee Model
+        public Employee Bind(String row)
+        {
+            string[] col = row.Split(Options.CsvDelimeter);
+            Employee employee = new Employee();
+            PropertyInfo[] propertyInfo = typeof(Employee).GetProperties();
+            int propertyIndex = 0;
+            foreach (PropertyInfo property in propertyInfo)
+            {
+                if (property.Name != propertyInfo[0].Name)
+                {
+                    property.SetValue(employee, Convert.ChangeType(col[propertyIndex], property.PropertyType), null);
+                    propertyIndex++;
+                }
+            }
+            return employee;
         }
     }
 }
