e6916f923f88fd2810cf210cb5bf8b47473164dd
AngeloRWolff <45687159+AngeloRWolff@users.noreply.github.com>
2020-10-24 10:06:38 +0200

    Added Model 'SeedDataProperties'

---
+++ OrganizationX/OrganizationX/Models/SeedDataProperties.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrganizationX.Models
+{
+    public class SeedDataProperties
+    {
+        public Queue<Employee> Employees { get; set; }
+      
+        public int TotalRecords { get; set; }
+
+        //Psuedo - In process
+        public List<Key> MissingKeys { get; set; }
+        public List<EmployeeConflicts> Conflicts { get; set; }
+    }
+
+    public class Key
+    {
+        public string KeyName { get; set; }
+    }
+
+    public class EmployeeConflicts
+    {
+        public Employee originEmployee;
+        public Employee conflictEmployee;
+    }
+}
