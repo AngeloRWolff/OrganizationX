a1ab0f0e33b2c985bbddfe61ef4bea05c9dc41e1
AngeloRWolff <45687159+AngeloRWolff@users.noreply.github.com>
2020-10-24 07:57:00 +0200

    Added ApplicationDbContext Migration

--- OrganizationX/OrganizationX/Data/Migrations/20201024055629_ApplicationDb.Designer.cs
+++ OrganizationX/OrganizationX/Data/Migrations/20201024055629_ApplicationDb.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrganizationX.Data;
 
 namespace OrganizationX.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201024055629_ApplicationDb")]
+    partial class ApplicationDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OrganizationX/OrganizationX/Data/Migrations/20201024055629_ApplicationDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrganizationX.Data.Migrations
+{
+    public partial class ApplicationDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
