6a178a60dbb16d436a9c7a113ee966bfdd1d9388
AngeloRWolff <45687159+AngeloRWolff@users.noreply.github.com>
2020-10-24 09:30:29 +0200

    Created Function 'openCsv'
    +File Reader function -> Reads file from html 5 input type of file url

--- OrganizationX/OrganizationX/wwwroot/js/site.js
+++ OrganizationX/OrganizationX/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function openCsv(event) {
+    var input = event.target;
+    var reader = new FileReader();
+    var textField = document.getElementById('loadTextField');
+    reader.onload = function () {
+        var data = reader.result;
+        textField.innerHTML = atob(data.split(',')[1]);
+    };
+    reader.readAsDataURL(input.files[0]);
+
+};
