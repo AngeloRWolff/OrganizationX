b1c4628f7caa89f3ad4644e1a6b10a211d3334a0
AngeloRWolff <45687159+AngeloRWolff@users.noreply.github.com>
2020-10-24 07:11:06 +0200

    Added Employee Properties

--- OrganizationX/OrganizationX/Models/Employee.cs
+++ OrganizationX/OrganizationX/Models/Employee.cs
@@ -7,5 +7,40 @@ namespace OrganizationX.Models
 {
     public class Employee
     {
+        public int Age { get; set; }
+        public int Attrition { get; set; }
+        public int BusinessTravel { get; set; }
+        public int DailyRate { get; set; }
+        public int Department { get; set; }
+        public int DistanceFromHome { get; set; }
+        public int Education { get; set; }
+        public int EducationField { get; set; }
+        public int EmployeeCount { get; set; }
+        public int EmployeeNumber { get; set; }
+        public int EnvironmentSatisfaction { get; set; }
+        public int Gender { get; set; }
+        public int HourlyRate { get; set; }
+        public int JobInvolvement { get; set; }
+        public int JobLevel { get; set; }
+        public int JobRole { get; set; }
+        public int JobSatisfaction { get; set; }
+        public int MaritalStatus { get; set; }
+        public int MonthlyIncome { get; set; }
+        public int MonthlyRate { get; set; }
+        public int NumCompaniesWorked { get; set; }
+        public int Over18 { get; set; }
+        public int OverTime { get; set; }
+        public int PercentSalaryHike { get; set; }
+        public int PerformanceRating { get; set; }
+        public int RelationshipSatisfaction { get; set; }
+        public int StandardHours { get; set; }
+        public int StockOptionLevel { get; set; }
+        public int TotalWorkingYears { get; set; }
+        public int TrainingTimesLastYear { get; set; }
+        public int WorkLifeBalance { get; set; }
+        public int YearsAtCompany { get; set; }
+        public int YearsInCurrentRole { get; set; }
+        public int YearsSinceLastPromotion { get; set; }
+        public int YearsWithCurrManager { get; set; }
     }
 }
