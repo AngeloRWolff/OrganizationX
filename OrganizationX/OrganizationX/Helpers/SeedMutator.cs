b42f3b165fc71855bea8792a624ac8cac64d1b30
AngeloRWolff <45687159+AngeloRWolff@users.noreply.github.com>
2020-10-24 09:49:11 +0200

    Added Class 'CharMapper'

--- OrganizationX/OrganizationX/Helpers/SeedMutator.cs
+++ OrganizationX/OrganizationX/Helpers/SeedMutator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrganizationX.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,13 @@ namespace OrganizationX.Helpers
     {
         public string SeedData { get; set; }
         public SeedOptions Options { get; set; }
+
+        public Queue<Employee> GetEmployees()
+        {
+            Queue<Employee> newEmployees = new Queue<Employee>();
+
+            return newEmployees;
+        }
     }
 }
 
@@ -16,4 +24,19 @@ public class SeedOptions
 {
     public bool Keyless { get; set; }
     public char CsvDelimeter { get; set; } = ','; //As defualt 
+}
+
+public class CharMapper
+{
+    public string Row { get; set; } = String.Empty;
+
+    public void ClearRow()
+    {
+        Row = String.Empty;
+    }
+
+    public void Append(char character)
+    {
+        Row += character;
+    }
 }
