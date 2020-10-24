9156125ee44b68e3105591bf4c330d89058ed279
AngeloRWolff <45687159+AngeloRWolff@users.noreply.github.com>
2020-10-24 09:46:04 +0200

    Updated SeedMutator
    +Added Properties
    +Added Class 'SeedOptions'

--- OrganizationX/OrganizationX/Helpers/SeedMutator.cs
+++ OrganizationX/OrganizationX/Helpers/SeedMutator.cs
@@ -7,6 +7,13 @@ namespace OrganizationX.Helpers
 {
     public class SeedMutator
     {
-
+        public string SeedData { get; set; }
+        public SeedOptions Options { get; set; }
     }
+}
+
+public class SeedOptions
+{
+    public bool Keyless { get; set; }
+    public char CsvDelimeter { get; set; } = ','; //As defualt 
 }
