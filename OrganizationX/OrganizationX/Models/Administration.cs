59675d4ba527753e65adacdbb47ff645a641ef13
AngeloRWolff <45687159+AngeloRWolff@users.noreply.github.com>
2020-11-02 12:00:21 +0200

    Added partial Model Data to 'Administration'

--- OrganizationX/OrganizationX/Models/Administration.cs
+++ OrganizationX/OrganizationX/Models/Administration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace OrganizationX.Models
 {
     public class Administration
     {
+        [Key]
+        public int Id { get; set; }
+
+        public String Username { get; set; }
+
+        public String PasswordHash { get; set; }
+
+        [EmailAddress]
+        public String Email { get; set; }
     }
 }
